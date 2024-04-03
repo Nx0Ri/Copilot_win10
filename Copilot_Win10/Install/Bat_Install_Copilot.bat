a0bd78906c379a80cde5de2deddbcd7462636c6c
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-03 12:37:06 +0700

    Add files via upload

--- Copilot_Win10/Install/Bat_Install_Copilot.bat
+++ Copilot_Win10/Install/Bat_Install_Copilot.bat
@@ -1,6 +1,8 @@
 @echo off
+copy Reg_Install_Copilot.reg %HOMEDRIVE%\Windows
 cd ViVe
 vivetool /enable /id:46686174,47530616,44755019
+regedit /s autorun_copilot.reg
 echo .
 echo ------------------------------------------
 echo Done! Now RESTART PC and run Copilot.reg
