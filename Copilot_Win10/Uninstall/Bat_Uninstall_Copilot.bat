0944beeaafa24bea553e480e0aadcb3715d1c22c
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-03 12:08:53 +0700

    last update

---
+++ Copilot_Win10/Uninstall/Bat_Uninstall_Copilot.bat
@@ -0,0 +1,9 @@
+@echo off
+cd ViVe
+vivetool /disable /id:46686174,47530616,44755019
+echo .
+echo ------------------------------------------
+echo Done! Now restart pc and everything done
+echo ------------------------------------------
+echo .
+pause;
