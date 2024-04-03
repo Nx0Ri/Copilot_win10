c5c8805908065b29c638bb0a5a22f7385f93ed09
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-03 12:01:55 +0700

    Add files via upload

---
+++ Copilot_Win10/Install/Install_Copilot.bat
@@ -0,0 +1,9 @@
+@echo off
+cd ViVe
+vivetool /enable /id:46686174,47530616,44755019
+echo .
+echo ------------------------------------------
+echo Done! Now RESTART PC and run Copilot.reg
+echo ------------------------------------------
+echo .
+pause;
---
+++ Copilot_Win10/Uninstall/Uninstall_Copilot.bat
@@ -0,0 +1,9 @@
+@echo off
+cd ViVe
+vivetool /disable /id:46686174,47530616,44755019
+echo .
+echo ------------------------------------------
+echo Done! Now run Uninstall_Copilot.reg and restart pc
+echo ------------------------------------------
+echo .
+pause;
