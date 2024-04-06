e71ae482558253dfe090d55ec3082c3be2cbef56
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-06 22:02:35 +0700

    Add files via upload

---
+++ Copilot_Win10/Uninstall/Disable_Fix_Region_Copilot.bat
@@ -0,0 +1,40 @@
+@echo off
+echo[
+echo Editing registry...
+echo -------------------
+reg delete HKCU\SOFTWARE\Policies\Microsoft\Edge /v DnsOverHttpsMode
+reg delete HKCU\SOFTWARE\Policies\Microsoft\Edge /v DnsOverHttpsTemplates
+echo[
+echo[
+echo Updating registry policies for Microsoft Edge...
+echo -------------------
+gpupdate /force /target:computer
+echo ----------------------------------------------------------------------------
+echo Done! Changed Microsoft Edge DNS settings for Copilot. 
+echo You cant use Copilot now. (If you have regional block.)   
+echo Use Install/3_Fix_Region_Copilot.bat for enabling DNS back in Microsoft Edge                            
+echo ----------------------------------------------------------------------------
+echo[
+timeout 1 > NUL /NOBREAK
+echo Wait 10 sec for closing app...
+timeout 1 > NUL /NOBREAK
+echo 9 sec...
+timeout 1 > NUL /NOBREAK
+echo 8 sec...
+timeout 1 > NUL /NOBREAK
+echo 7 sec...
+timeout 1 > NUL /NOBREAK
+echo 6 sec...
+timeout 1 > NUL /NOBREAK
+echo 5 sec...
+timeout 1 > NUL /NOBREAK
+echo 4 sec...
+timeout 1 > NUL /NOBREAK
+echo 3 sec...
+timeout 1 > NUL /NOBREAK
+echo 2 sec...
+timeout 1 > NUL /NOBREAK
+echo 1 sec...
+timeout 1 > NUL /NOBREAK
+echo Goodbye :D
+timeout 1 > NUL /NOBREAK
