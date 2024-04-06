3b3d74ff82dbe790b7c4aaceb595dfea18a118e9
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-06 22:04:52 +0700

    outdated

--- Copilot_Win10/Install/Install_Copilot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ViVe
-vivetool /enable /id:46686174,47530616,44755019
-echo .
-echo ------------------------------------------
-echo Done! Now RESTART PC and run Copilot.reg
-echo ------------------------------------------
-echo .
-pause;
