0927e1b6c09c598e99656652bca668db9b3e619e
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-06 22:06:01 +0700

    outdated

--- Copilot_Win10/Uninstall/Bat_Uninstall_Copilot.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd ViVe
-vivetool /disable /id:46686174,47530616,44755019
-echo .
-echo ------------------------------------------
-echo Done! Now restart pc and everything done
-echo ------------------------------------------
-echo .
-pause;
