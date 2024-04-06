733359085edc23057df86c74223eb80e34887855
Ori <152011726+Nx0Ri@users.noreply.github.com>
2024-04-06 22:48:17 +0700

    finally

---
+++ Copilot_Win10/Install/1_Install_Copilot.bat
@@ -0,0 +1,21 @@
+@echo off
+cd ViVe
+vivetool /enable /id:46686174,47530616,44755019
+echo[
+echo ----------------------------------------------
+echo Done! Now RESTART PC and run 2_Fix_Copilot.bat
+echo ----------------------------------------------
+echo[
+timeout 1 > NUL /NOBREAK
+echo Wait 5 sec for closing app...
+timeout 1 > NUL /NOBREAK
+echo 4 sec...
+timeout 1 > NUL /NOBREAK
+echo 3 sec...
+timeout 1 > NUL /NOBREAK
+echo 2 sec...
+timeout 1 > NUL /NOBREAK
+echo 1 sec...
+timeout 1 > NUL /NOBREAK
+echo Goodbye :D
+timeout 1 > NUL /NOBREAK
